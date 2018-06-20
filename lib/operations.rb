79d7fcaed1a55335c96193f3ac114ee56ddf8d8e
Justine Bricknell <justinegayton@gmail.com>
2018-06-20 18:32:45 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+if speed == speed.between?(40, 60)
+	return true
+else
+	return false 
 end
-
+end
 
 
 def not_safe?(speed)
-	
+	if speed > 60 && speed < 40 ? true : false
 end
-	
-
-
+end
